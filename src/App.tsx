9dd8e38f3260ee7a7210267adf3e7186e4d18829
Dominik <dominik.wozniewicz@gmail.com>
2021-04-10 12:42:42 +0200

    Added all the boiler plate to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+// Components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+// Styles
+import { Wrapper } from "./App.styles";
+
 const App = () => {
   return (
     <div>
